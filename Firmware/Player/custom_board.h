2bf4d851b88b5eda138b109269363d9d469f3f91
Bud <budarriola@gmail.com>
2019-02-02 01:02:15 -0800

    changed led defines

--- Firmware/Player/custom_board.h
+++ Firmware/Player/custom_board.h
@@ -47,25 +47,23 @@ extern "C" {
 #include "nrf_gpio.h"
 
 // LEDs definitions for PCA10056
-#define LEDS_NUMBER    4
+#define LEDS_NUMBER    2
 
-#define LED_1          NRF_GPIO_PIN_MAP(0,13)
-#define LED_2          NRF_GPIO_PIN_MAP(0,14)
-#define LED_3          NRF_GPIO_PIN_MAP(0,15)
-#define LED_4          NRF_GPIO_PIN_MAP(0,16)
+#define LED_1          NRF_GPIO_PIN_MAP(0,12)
+#define LED_2          NRF_GPIO_PIN_MAP(0,105)
+//#define LED_3          NRF_GPIO_PIN_MAP(0,15)
+//#define LED_4          NRF_GPIO_PIN_MAP(0,16)
 #define LED_START      LED_1
-#define LED_STOP       LED_4
+#define LED_STOP       LED_2
 
-#define LEDS_ACTIVE_STATE 0
+#define LEDS_ACTIVE_STATE 1
 
-#define LEDS_LIST { LED_1, LED_2, LED_3, LED_4 }
+#define LEDS_LIST { LED_1, LED_2 }
 
 #define LEDS_INV_MASK  LEDS_MASK
 
-#define BSP_LED_0      13
-#define BSP_LED_1      14
-#define BSP_LED_2      15
-#define BSP_LED_3      16
+#define BSP_LED_0      12
+#define BSP_LED_1      105
 
 #define BUTTONS_NUMBER 4
 
